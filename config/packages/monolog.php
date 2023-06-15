be67959317d9cd61a01af130d31489245cf68498
Kevin <kevinbalicot@gmail.com>
2023-06-15 20:00:39 +0200

    chore: Add log for production

--- config/packages/monolog.php
+++ config/packages/monolog.php
@@ -9,6 +9,25 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         'channels' => ['deprecation'],
     ]);
 
+    if ('prod' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('monolog', [
+            'handlers' => [
+                'main' => [
+                    'type' => 'fingers_crossed',
+                    'action_level' => 'error',
+                    'handler' => 'nested',
+                    'excluded_http_codes' => [404, 405],
+                    'channels' => ['!event'],
+                ],
+                'nested' => [
+                    'type' => 'stream',
+                    'path' => '%kernel.logs_dir%/%kernel.environment%.log',
+                    'level' => 'info',
+                ],
+            ],
+        ]);
+    }
+
     if ('dev' === $containerConfigurator->env()) {
         $containerConfigurator->extension('monolog', [
             'handlers' => [
